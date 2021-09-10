9641c595668eb33a95ca3f535fcfedf380e76299
Christopher <khseredin@phoenixit.ru>
2021-09-10 15:10:09 +0300

    fixed: refactoring code

--- src/store/login/actions.js
+++ src/store/login/actions.js
@@ -19,7 +19,7 @@ export const fetchLogin = (username, password) => {
     store.dispatch(fetchDataStart());
 
 
-    return fetch('http://194.87.214.215/login', {
+    return fetch('http://194.87.239.49/api/login', {
             method: 'POST',
             mode: "cors",
             headers: {
